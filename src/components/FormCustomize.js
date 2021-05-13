7d71a95302177e6f1e0d6502064c81620a0749fb
nuevo-rama <ramiromelero@gmail.com>
2021-05-13 20:44:13 -0300

    actualizo

---
+++ src/components/FormCustomize.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+import CustomizeVisor from './components/CustomizeVisor';
+
+
+
+function FormCustomize() {
+  return (
+    <div className="App">
+      
+      
+    </div>
+  );
+}
+
+export default FormCustomize;
