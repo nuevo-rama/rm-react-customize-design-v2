0cb670e3207ce3f04fcb5d7551b576873fb0520b
nuevo-rama <ramiromelero@gmail.com>
2021-05-14 09:58:40 -0300

    agrupo todo en un solo component para lograr un solo form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 import CustomizeVisor from './components/CustomizeVisor';
-import CustomizePanel from './components/CustomizePanel';
-import CustomizeThread from './components/CustomizeThread';
-import CustomizeLogo from './components/CustomizeLogo';
-import CustomizeSticker from './components/CustomizeSticker';
+
 
 
 import './App.css';
@@ -12,7 +9,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
       <CustomizeVisor 
       inicialImage= {'/productImagesCustomize/cap-image-inicial.png'}
       VisorColorBlack= {'/productImagesCustomize/cap-image-visor-color-black.png'}
@@ -21,38 +17,26 @@ function App() {
       VisorColorMagenta= {'/productImagesCustomize/cap-image-visor-color-magenta.png'}
       VisorColorPink= {'/productImagesCustomize/cap-image-visor-color-pink.png'}
       VisorColorYellow= {'/productImagesCustomize/cap-image-visor-color-yellow.png'}
-      />
-
-      <CustomizePanel
-      inicialImage= {'/productImagesCustomize/cap-image-inicial.png'}
+      
       PanelColorBlack= {'/productImagesCustomize/cap-image-panels-color-black.png'}
       PanelColorBlue= {'/productImagesCustomize/cap-image-panels-color-blue.png'}
       PanelColorGrey= {'/productImagesCustomize/cap-image-panels-color-grey.png'}
       PanelColorMagenta= {'/productImagesCustomize/cap-image-panels-color-magenta.png'}
       PanelColorPink= {'/productImagesCustomize/cap-image-panels-color-pink.png'}
       PanelColorYellow= {'/productImagesCustomize/cap-image-panels-color-yellow.png'}
-      />
-
-      <CustomizeThread
-      inicialImage= {'/productImagesCustomize/cap-image-inicial.png'}
+      
       ThreadColorBlack= {'/productImagesCustomize/cap-image-thread-color-black.png'}
       ThreadColorBlue= {'/productImagesCustomize/cap-image-thread-color-blue.png'}
       ThreadColorGrey= {'/productImagesCustomize/cap-image-thread-color-grey.png'}
       ThreadColorMagenta= {'/productImagesCustomize/cap-image-thread-color-magenta.png'}
       ThreadColorPink= {'/productImagesCustomize/cap-image-thread-color-pink.png'}
       ThreadColorYellow= {'/productImagesCustomize/cap-image-thread-color-yellow.png'}
-      />
-
-      <CustomizeLogo
-      inicialImage= {'/productImagesCustomize/cap-image-inicial.png'}
+      
       LogoCentralBig= {'/productImagesCustomize/cap-image-logo-central-big.png'}
       LogoCentralSmall= {'/productImagesCustomize/cap-image-logo-central-small.png'}
       LogoClean= {'/productImagesCustomize/cap-image-logo-clean.png'}
       LogoPanelSide= {'/productImagesCustomize/cap-image-logo-panel-side.png'}
-      />
-
-      <CustomizeSticker
-      inicialImage= {'/productImagesCustomize/cap-image-inicial.png'}
+      
       StickerColorBlack= {'/productImagesCustomize/cap-image-sticker-color-black.png'}
       StickerColorBlue= {'/productImagesCustomize/cap-image-sticker-color-blue.png'}
       StickerColorGrey= {'/productImagesCustomize/cap-image-sticker-color-grey.png'}
@@ -60,8 +44,6 @@ function App() {
       StickerColorPink= {'/productImagesCustomize/cap-image-sticker-color-pink.png'}
       StickerColorYellow= {'/productImagesCustomize/cap-image-sticker-color-yellow.png'}
       />
-
-
     </div>
   );
 }
--- src/components/CustomizeVisor.js
+++ src/components/CustomizeVisor.js
@@ -6,32 +6,123 @@ constructor (props) {
     super (props);
     this.state = {
         inicialImage: props.inicialImage,
-        changeImage: props.changeImage,  
-    }
+        changeImage: props.changeImage,
+        value: ''  //para el form
+    };
+
+    this.handleChange = this.handleChange.bind(this); //para el form
+    this.handleSubmit = this.handleSubmit.bind(this); //para el form
 }
+
+handleChange(event) { //para el form
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) { //para el form
+    //alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  //VISOR
 
 VisorColorBlack () {
     this.setState ( {changeImage: this.props.VisorColorBlack})
 }
-
 VisorColorBlue () {
     this.setState ( {changeImage: this.props.VisorColorBlue})
 }
-
 VisorColorGrey () {
     this.setState ( {changeImage: this.props.VisorColorGrey})
 }
-
 VisorColorMagenta () {
     this.setState ( {changeImage: this.props.VisorColorMagenta})
 }
-
 VisorColorPink () {
     this.setState ( {changeImage: this.props.VisorColorPink})
 }
-
 VisorColorYellow () {
     this.setState ( {changeImage: this.props.VisorColorYellow})
+}
+
+//PANEL
+
+PanelColorBlack () {
+    this.setState ( {changeImage: this.props.PanelColorBlack})
+}
+PanelColorBlue () {
+    this.setState ( {changeImage: this.props.PanelColorBlue})
+}
+PanelColorGrey () {
+    this.setState ( {changeImage: this.props.PanelColorGrey})
+}
+PanelColorMagenta () {
+    this.setState ( {changeImage: this.props.PanelColorMagenta})
+}
+PanelColorPink () {
+    this.setState ( {changeImage: this.props.PanelColorPink})
+}
+PanelColorYellow () {
+    this.setState ( {changeImage: this.props.PanelColorYellow})
+}
+
+//LOGO
+
+LogoCentralBig () {
+    this.setState ( {changeImage: this.props.LogoCentralBig})
+}
+LogoCentralSmall () {
+    this.setState ( {changeImage: this.props.LogoCentralSmall})
+}
+LogoClean () {
+    this.setState ( {changeImage: this.props.LogoClean})
+}
+LogoPanelSide () {
+    this.setState ( {changeImage: this.props.LogoPanelSide})
+}
+LogoVisor () {
+    this.setState ( {changeImage: this.props.LogoVisor})
+}
+
+//THREAD
+
+ThreadColorBlack () {
+    this.setState ( {changeImage: this.props.ThreadColorBlack})
+}
+ThreadColorBlue () {
+    this.setState ( {changeImage: this.props.ThreadColorBlue})
+}
+ThreadColorGrey () {
+    this.setState ( {changeImage: this.props.ThreadColorGrey})
+}
+ThreadColorMagenta () {
+    this.setState ( {changeImage: this.props.ThreadColorMagenta})
+}
+ThreadColorPink () {
+    this.setState ( {changeImage: this.props.ThreadColorPink})
+}
+ThreadColorYellow () {
+    this.setState ( {changeImage: this.props.ThreadColorYellow})
+}
+
+//STICKER
+
+StickerColorBlack () {
+    this.setState ( {changeImage: this.props.StickerColorBlack})
+}
+StickerColorBlue () {
+    this.setState ( {changeImage: this.props.StickerColorBlue})
+}
+StickerColorGrey () {
+    this.setState ( {changeImage: this.props.StickerColorGrey})
+}
+StickerColorMagenta () {
+    this.setState ( {changeImage: this.props.StickerColorMagenta})
+}
+StickerColorPink () {
+    this.setState ( {changeImage: this.props.StickerColorPink})
+}
+StickerColorYellow () {
+    this.setState ( {changeImage: this.props.StickerColorYellow})
 }
 
 resetImagen () {
@@ -40,24 +131,62 @@ resetImagen () {
 
 render() {                         //ENVUELVO TODO EN RENDER
 return (
+    
       <div className= "frame" >
+          
           <div className= "frameLeft">
             <img className = "positionFixed" src ={this.state.inicialImage}></img>
             <img className = "positionFixed" src ={this.state.changeImage}></img>
           </div>
+
           <div className= "frameRight">
-          <button id = "black" onClick= { () => this.VisorColorBlack() }>Negro</button>
-          <button id = "blue" onClick= { () => this.VisorColorBlue() }>Azul</button>
+            <form onSubmit={this.handleSubmit}>
+                <label>Elige un color para la Visera</label>
+                <input type="radio" id = "black" name= "visor" value={this.state.value} onChange= { () => this.VisorColorBlack() }/>
+                <input type="radio" id = "blue" name= "visor" value={this.state.value} onChange= { () => this.VisorColorBlue() }/>
+                <input type="radio" id = "grey" name= "visor" value={this.state.value} onChange= { () => this.VisorColorGrey() }/>
+                <input type="radio" id = "magenta" name= "visor" value={this.state.value} onChange= { () => this.VisorColorMagenta() }/>
+                <input type="radio" id = "pink" name= "visor" value={this.state.value} onChange= { () => this.VisorColorPink() }/>
+                <input type="radio" id = "yellow" name= "visor" value={this.state.value} onChange= { () => this.VisorColorYellow() }/>
 
-          <button id = "grey" onClick= { () => this.VisorColorGrey() }>Gris</button>
-          <button id = "magenta" onClick= { () => this.VisorColorMagenta() }>Magenta</button>
+                <label>Elige un color para los Paneles</label>
+                <input type="radio" id = "black" name= "panel" value={this.state.value} onChange= { () => this.PanelColorBlack() }/>
+                <input type="radio" id = "blue" name= "panel" value={this.state.value} onChange= { () => this.PanelColorBlue() }/>
+                <input type="radio" id = "grey" name= "panel" value={this.state.value} onChange= { () => this.PanelColorGrey() }/>
+                <input type="radio" id = "magenta" name= "panel" value={this.state.value} onChange= { () => this.PanelColorMagenta() }/>
+                <input type="radio" id = "pink" name= "panel" value={this.state.value} onChange= { () => this.PanelColorPink() }/>
+                <input type="radio" id = "yellow" name= "panel" value={this.state.value} onChange= { () => this.PanelColorYellow() }/>
 
-          <button id = "pink" onClick= { () => this.VisorColorPink() }>Rosa</button>
-          <button id = "yellow" onClick= { () => this.VisorColorYellow() }>Amarillo</button>
-          
-          <button id = "reset" onClick= { () => this.resetImagen() }>Reset</button>
+                <label>Elige el tamaño y posición del Logo</label>
+                <input type="radio" id = "logoSelect" name= "logo" value={this.state.value} onChange= { () => this.LogoCentralBig() }/>
+                <input type="radio" id = "logoSelect" name= "logo" value={this.state.value} onChange= { () => this.LogoCentralSmall() }/>
+                <input type="radio" id = "logoSelect" name= "logo" value={this.state.value} onChange= { () => this.LogoClean() }/>
+                <input type="radio" id = "logoSelect" name= "logo" value={this.state.value} onChange= { () => this.LogoPanelSide() }/>
+                <input type="radio" id = "logoSelect" name= "logo" value={this.state.value} onChange= { () => this.LogoVisor() }/>
+
+                <label>Elige un color para las Costuras</label>
+                <input type="radio" id = "black" name= "thread" value={this.state.value} onChange= { () => this.ThreadColorBlack() }/>
+                <input type="radio" id = "blue" name= "thread" value={this.state.value} onChange= { () => this.ThreadColorBlue() }/>
+                <input type="radio" id = "grey" name= "thread" value={this.state.value} onChange= { () => this.ThreadColorGrey() }/>
+                <input type="radio" id = "magenta" name= "thread" value={this.state.value} onChange= { () => this.ThreadColorMagenta() }/>
+                <input type="radio" id = "pink" name= "thread" value={this.state.value} onChange= { () => this.ThreadColorPink() }/>
+                <input type="radio" id = "yellow" name= "thread" value={this.state.value} onChange= { () => this.ThreadColorYellow() }/>
+
+                <label>Elige un color para el Sticker</label>
+                <input type="radio" id = "black" name= "sticker" value={this.state.value} onChange= { () => this.StickerColorBlack() }/>
+                <input type="radio" id = "blue" name= "sticker" value={this.state.value} onChange= { () => this.StickerColorBlue() }/>
+                <input type="radio" id = "grey" name= "sticker" value={this.state.value} onChange= { () => this.StickerColorGrey() }/>
+                <input type="radio" id = "magenta" name= "sticker" value={this.state.value} onChange= { () => this.StickerColorMagenta() }/>
+                <input type="radio" id = "pink" name= "sticker" value={this.state.value} onChange= { () => this.StickerColorPink() }/>
+                <input type="radio" id = "yellow" name= "sticker" value={this.state.value} onChange= { () => this.StickerColorYellow() }/>
+                
+                
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+
       </div>
-      </div>
+      
   )
 
 }}
